9da781b283e0111d73c43a94a0d5db85fb8a2221
otoukebri <oussema.toukebri@gmail.com>
2019-05-05 17:10:39 +0200

    feat(reactiveForm) formControl example

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  login = new FormControl('');
+  password = new FormControl('');
+
+
+constructor() { }
+
+ngOnInit() {
+}
+
+authenticate() {
+  console.log('authenticate clicked');
+  this.login.setValue('invalid login');
+}
+
+}
