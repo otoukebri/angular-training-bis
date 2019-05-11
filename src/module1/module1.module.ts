9e34addfeece2f382c969b8f99c8dcab08a0a370
otoukebri <oussema.toukebri@gmail.com>
2019-05-11 17:21:11 +0200

    seveal modules to emulate routing

---
+++ src/module1/module1.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component2Component } from './component2/component2.component';
+
+@NgModule({
+  declarations: [Component2Component],
+  imports: [
+    CommonModule
+  ]
+})
+export class Module1Module { }
