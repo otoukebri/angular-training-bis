5640b38664ce162330888cd0645d29e71a59b815
otoukebri <oussema.toukebri@gmail.com>
2019-05-12 16:57:39 +0200

    move components to /components directroy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app.component';
 import { Component2Component } from './component2/component2.component';
 import { Component3Component } from 'src/module1/component3/component3.component';
 import { Component2DetailComponent } from './component2-detail/component2-detail.component';
+import { GuardSampleService } from './services/guard-sample.service';
 
 const routes: Routes = [ {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: AppComponent },
-  {path : 'cmp2', component: Component2Component},
+  {path : 'cmp2', component: Component2Component, canActivate: [GuardSampleService]},
   {path : 'cmp2/:id', component: Component2DetailComponent},
   {path : 'cmp3', component: Component3Component},
   {path : '**', component: AppComponent}
