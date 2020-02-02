f12d93c4d27cb3849b174141bea8f39b202cb864
otoukebri <oussema.toukebri@gmail.com>
2020-02-02 18:54:10 +0100

    add nested form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,37 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit /*, OnChanges*/ {
 
-  loginForm: FormGroup;
+  registerForm: FormGroup;
 
   ngOnInit() {
     // register login FormGroup
-    this.loginForm = new FormGroup({
+    this.registerForm = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
+      adress: new FormGroup({
+        city: new FormControl(''),
+        zipCode: new FormControl('')
+      })
     });
 
-    // get all form control values
-    console.log('ngOnInit.subscriptionForm.getRawValue ' +  JSON.stringify(this.loginForm.getRawValue()));
+    // get all form control values even hidden fields
+    console.log('ngOnInit.subscriptionForm.getRawValue ' + JSON.stringify(this.registerForm.getRawValue()));
 
     // email form value
-    console.log('ngOnInitfirstName: ' +  this.loginForm.get('email').value);
+    console.log('ngOnInitfirstName: ' + this.registerForm.get('email').value);
 
     // set form value
-    this.loginForm.setValue({
-      email : 'login@domain.com',
-      password: 'secret-password'
+    this.registerForm.setValue({
+      email: 'login@domain.com',
+      password: 'secret-password',
+      adress: {
+        city: 'Tunis',
+        zipCode: '2010'
+      }
     });
 
     // set email value
-    this.loginForm.get('email').patchValue('login@domain.com');
+    this.registerForm.get('email').patchValue('login@domain.com');
 
     // subscribe to form changes
     this.onValueChanges();
@@ -39,13 +47,13 @@ export class AppComponent implements OnInit /*, OnChanges*/ {
 
   onValueChanges(): void {
     // email FormControl changes
-    this.loginForm.get('email').valueChanges.subscribe( emailValue => {
-      console.log('x1: ' +  emailValue);
+    this.registerForm.get('email').valueChanges.subscribe(emailValue => {
+      console.log('x1: ' + emailValue);
     });
 
     // all form control changes
-    this.loginForm.valueChanges.subscribe( loginForm => {
-      console.log('x1: ' +  JSON.stringify(loginForm));
+    this.registerForm.valueChanges.subscribe(loginForm => {
+      console.log('x1: ' + JSON.stringify(loginForm));
     });
 
   }
