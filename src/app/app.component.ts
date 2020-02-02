c53e29409205d23bf7592dba7c22b15f702bc3fb
otoukebri <oussema.toukebri@gmail.com>
2020-02-02 15:43:30 +0100

    form control methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,36 +11,42 @@ export class AppComponent implements OnInit /*, OnChanges*/ {
   loginForm: FormGroup;
 
   ngOnInit() {
+    // register login FormGroup
     this.loginForm = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
     });
 
+    // get all form control values
     console.log('ngOnInit.subscriptionForm.getRawValue ' +  JSON.stringify(this.loginForm.getRawValue()));
 
+    // email form value
     console.log('ngOnInitfirstName: ' +  this.loginForm.get('email').value);
 
+    // set form value
+    this.loginForm.setValue({
+      email : 'login@domain.com',
+      password: 'secret-password'
+    });
+
+    // set email value
+    this.loginForm.get('email').patchValue('login@domain.com');
+
+    // subscribe to form changes
     this.onValueChanges();
 
   }
 
   onValueChanges(): void {
-    // only firstNameControl changes
-    this.loginForm.get('email').valueChanges.subscribe( x => {
-      console.log('x1: ' +  x);
+    // email FormControl changes
+    this.loginForm.get('email').valueChanges.subscribe( emailValue => {
+      console.log('x1: ' +  emailValue);
     });
 
     // all form control changes
-    this.loginForm.valueChanges.subscribe( x => {
-      console.log('x1: ' +  JSON.stringify(x));
+    this.loginForm.valueChanges.subscribe( loginForm => {
+      console.log('x1: ' +  JSON.stringify(loginForm));
     });
 
   }
-
-  // public ngOnChanges(changes: SimpleChanges) {
-
-  //   // if ('firstName' in changes) {
-  //   //   console.log('v: ' + v);
-  //   // }
-  // }
 }
