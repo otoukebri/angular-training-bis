4b72e208dd7addffac304913f0ff478740127ead
otoukebri <oussema.toukebri@gmail.com>
2020-02-02 14:46:19 +0100

    sample form value chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-training-bis';
+export class AppComponent implements OnInit /*, OnChanges*/ {
+
+  loginForm: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
+
+    console.log('ngOnInit.subscriptionForm.getRawValue ' +  JSON.stringify(this.loginForm.getRawValue()));
+
+    console.log('ngOnInitfirstName: ' +  this.loginForm.get('email').value);
+
+    this.onValueChanges();
+
+  }
+
+  onValueChanges(): void {
+    // only firstNameControl changes
+    this.loginForm.get('email').valueChanges.subscribe( x => {
+      console.log('x1: ' +  x);
+    });
+
+    // all form control changes
+    this.loginForm.valueChanges.subscribe( x => {
+      console.log('x1: ' +  JSON.stringify(x));
+    });
+
+  }
+
+  // public ngOnChanges(changes: SimpleChanges) {
+
+  //   // if ('firstName' in changes) {
+  //   //   console.log('v: ' + v);
+  //   // }
+  // }
 }
